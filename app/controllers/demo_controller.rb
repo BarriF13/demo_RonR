3a9db7c4a581a12efa37b5d1011947ed2189c5d7
Barri <info@bdesignstudio.co.uk>
2020-03-16 18:18:03 +0000

    Link types

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -3,12 +3,12 @@ class DemoController < ApplicationController
   layout false
   
   def index
-    # render(:template => 'demo/hello')
-    render('demo/hello')
+  
   end
 
   def hello
-    render('demo/index')
+    # render('demo/index')
+    @array = [1,2,3,4,5]
   end
 
   def other_hello
